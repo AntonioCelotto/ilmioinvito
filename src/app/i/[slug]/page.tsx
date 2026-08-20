d260a43acf4d3b6f1eefa24e0d7acbb77fb303ce
AntonioCelotto <a.celotto@newdigitalapp.com>
2026-08-20 15:42:56 +0200

    Attiva correzioni responsive anteprima pubblica

--- src/app/i/[slug]/page.tsx
+++ src/app/i/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { DraftInviteClient } from "@/components/draft-invite-client";
 import { InviteStoryEnhancer } from "@/components/invite-story-enhancer";
 import { InviteCelebrationNumber } from "@/components/invite-celebration-number";
+import styles from "@/components/public-invite.module.css";
 
 type InvitePageProps = {
   params: Promise<{
@@ -12,10 +13,10 @@ export default async function InvitePage({ params }: InvitePageProps) {
   const { slug } = await params;
 
   return (
-    <>
+    <div className={styles.publicInvite}>
       <DraftInviteClient slug={slug} />
       <InviteStoryEnhancer slug={slug} />
       <InviteCelebrationNumber slug={slug} />
-    </>
+    </div>
   );
 }
